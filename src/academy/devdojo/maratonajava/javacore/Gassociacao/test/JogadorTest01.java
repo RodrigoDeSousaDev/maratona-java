ef6c3b2c59dfab393d3e27b2e738bbfad7b8c77d
RodrigoDeSousaDev <rodrigoprofissional912@hotmail.com>
2025-11-19 12:18:56 -0300

    associação arrays com objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("pelé");
+        Jogador jogador2 = new Jogador("romario");
+        Jogador jogador3 = new Jogador("messi");
+
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+       for (Jogador jogador: jogadores){
+           jogador.imprime();
+       }
+    }
+
+}
