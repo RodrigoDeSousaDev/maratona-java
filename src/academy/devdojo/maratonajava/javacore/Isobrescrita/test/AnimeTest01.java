8e8d54f903b1d91236dea59229a425175c215166
RodrigoDeSousaDev <rodrigoprofissional912@hotmail.com>
2025-11-20 13:12:20 -0300

    sobrescrita toString

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("tensei shitara slime datta");
+
+        System.out.println(anime);
+    }
+}
